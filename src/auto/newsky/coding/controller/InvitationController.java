69d143ac486098bf2fc32406e0f1474595794c55
270949894@qq.com <270949894@qq.com>
2016-09-16 21:03:29 +0800

    更新spring mvc.xml配置文件,去掉.action

--- src/auto/newsky/coding/controller/InvitationController.java
+++ src/auto/newsky/coding/controller/InvitationController.java
@@ -1,7 +1,27 @@
 package auto.newsky.coding.controller;
 
+import auto.newsky.coding.po.Invatation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Administrator on 2016/9/16.
  */
+@Controller
+@RequestMapping("/invitation")
 public class InvitationController {
+
+    /**
+     * 获取邀约列表
+     */
+    @RequestMapping("/getInvitations")
+    public void getInvitations(){
+
+    }
+    @ResponseBody
+    @RequestMapping("/publishInvitation")
+    public Invatation publishInvitation(Invatation invatation) throws Exception{
+        return invatation;
+    }
 }
