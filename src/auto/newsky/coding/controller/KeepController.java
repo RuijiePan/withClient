3540b68af6d5c39322144a9780cb0b707455d6c3
prj <zquprj@gmail.com>
2016-09-16 20:28:56 +0800

    Result增加

---
+++ src/auto/newsky/coding/controller/KeepController.java
@@ -0,0 +1,18 @@
+package auto.newsky.coding.controller;
+
+import auto.newsky.coding.response.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by prj on 2016/9/16.
+ */
+@Controller
+@RequestMapping("/keep/")
+public class KeepController {
+
+    /*@RequestMapping("createTask")
+    public Result createTask(String token,String title,String content,int index){
+
+    }*/
+}
---
+++ src/auto/newsky/coding/response/Result.java
@@ -0,0 +1,41 @@
+package auto.newsky.coding.response;
+
+/**
+ * Created by prj on 2016/9/16.
+ */
+public class Result {
+
+    private int code;
+    private String msg;
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Result(Object data) {
+        this.data = data;
+        this.msg = "";
+        this.code = 200;
+    }
+}
